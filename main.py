75fe2e2c63e1133c313ee62b98231f1c23968ae4
Abhishek Garg <abhishekgarg@Abhisheks-MacBook-Air.local>
2026-01-05 18:36:04 +0530

    added output parsing

--- main.py
+++ main.py
@@ -7,16 +7,27 @@ from langchain.agents import AgentExecuter
 from langchain.agents import create_react_agent
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch 
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+
+from schemas import AgentResponse
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS 
 
 
 tools=[TavilySearch()]
 llm=ChatOpenAI()
 react_prompt=hub.pull("hwchase17/react")
 
+output_parser = PydanticOutputParser (pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate (
+template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS, input_variables=["input", "agent_scratchpad", "tool_names"]
+) .partial(format_instructions=output_parser.get_format_instructions ())
+
 agent=create_react_agent(
     llm=llm,
     tools=tools,
-    prompt=react_prompt,
+    prompt=react_prompt_with_format_instructions
 )
 
 agent_executor = AgentExecuter(agent=agent, tools=tools, verbose=True)
